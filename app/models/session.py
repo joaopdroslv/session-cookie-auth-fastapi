6c7de4ee983d3dfc6556a16e49f18b72730da1d3
JOÃO PEDRO <joao.pedro@bevioficial.com.br>
2025-08-06 15:51:35 -0300

    session model updated

--- app/models/session.py
+++ app/models/session.py
@@ -1,9 +1,18 @@
+from datetime import datetime
+
 from database.database import Base
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Boolean
 
 
 class Session(Base):
     __tablename__ = "sessions"
 
     id = Column(String, primary_key=True, index=True)  # uuid
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    last_seen = Column(DateTime)
+    expired = Column(Boolean, default=False)
+
+    ip_address = Column(String, nullable=True)
+    user_agent = Column(String, nullable=True)
