10fb1bb4a2b1228ff2d1807c2da1d23edbc2486d
JOÃO PEDRO <joao.pedro@bevioficial.com.br>
2025-08-06 16:39:07 -0300

    database availability validation added

--- app/lifespan.py
+++ app/lifespan.py
@@ -1,5 +1,7 @@
 from contextlib import asynccontextmanager
+import time
 
+from sqlalchemy import text
 from database.database import Base, engine
 from fastapi import FastAPI
 from models.session import Session
@@ -9,7 +11,25 @@ from models.user import User
 @asynccontextmanager
 async def lifespan(app: FastAPI):
 
+    max_tries = 18  # 3 minutes of max waiting time
+    wait_seconds = 10
+
+    for attempt in range(max_tries):
+        try:
+            with engine.connect() as conn:
+                conn.execute(text("SELECT 1"))
+            print("Database is ready!")
+            break
+
+        except Exception as e:
+            print(f"Database not ready yet (attempt {attempt + 1}/{max_tries}): {e}")
+            time.sleep(wait_seconds)
+
+    else:
+        raise RuntimeError("Database did not become ready in time")
+
     print("Creating tables...")
     Base.metadata.create_all(bind=engine)
     print("Tables created successfully.")
+
     yield
